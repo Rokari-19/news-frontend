05910bbf9121ac552c1a5f042b826df27098d9b2
rokari-19 <oshomahiyelobu1@gmail.com>
2025-03-13 23:26:34 +0100

    changed base url to hosted backend

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://news-ghvj.onrender.com/'
 createApp(App).use(router).mount('#app')
